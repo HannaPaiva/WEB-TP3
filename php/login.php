cb0daf6abbc7ba2474094c9d40276248f9654628
Ferreira <paulocferreira@deloitte.pt>
2023-12-14 17:31:12 +0000

    finalizado o sistema de login

--- php/login.php
+++ php/login.php
@@ -4,7 +4,7 @@
       $email = $_POST["email"];
       $password = $_POST["password"];
   }
-    $sql = "SELECT email, password FROM utilizadores WHERE email = :email";
+    $sql = "SELECT * FROM utilizadores WHERE email = :email";
 
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':email', $email);
@@ -13,12 +13,19 @@
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($result) {
-        // Store email and password in variables
         $dbEmail = $result['email'];
         $dbPassword = $result['password'];
-
+            if ($email = $dbEmail && $password == $password){
+                session_start();
+                    if(isset($_SESSION["user"])){
+                        $_SESSION["user"] = $result["userid"];
+                        
+          }
+          
+        }
         echo "Email from database: $dbEmail<br>";
         echo "Password from database: $dbPassword";
+        header("Location: ../html/index.html");
     } else {
         echo "No matching record found for email: $email";
     }
