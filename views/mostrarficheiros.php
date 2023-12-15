c66738d010dd127f99079a5ec63a4c3e35fcbe91
HannaPaiva <hapaiva@deloitte.pt>
2023-12-15 11:53:59 +0000

    mudanças de ficheiro e inicialização das permissões, métodos de download, etc

--- views/mostrarficheiros.php
+++ views/mostrarficheiros.php
@@ -19,50 +19,106 @@ require "../html/components/head.html";
       <!-- Content wrapper -->
       <div class="content-wrapper">
         <!-- Content -->
-<section name="ver" style="text-align: center";>
-    <div>
-        <h2>Ver Ficheiros</h2>
-        <?php
-        $ficheiros = scandir("../assets/ficheiros");
-        for ($a = 2; $a < count($ficheiros); $a++) {
-            ?>
-            <p>
-                <a href="assets/ficheiros<?php echo $ficheiros[$a] ?>">
-                    <?php echo $ficheiros[$a] ?>
-                </a>
-            </p>
-            <?php
-        }
-        ?>
-    </div>
-    </section>
-<hr>
+        <div class="container-xxl flex-grow-1 container-p-y">
+          <div class="row">
+            <div class="card">
+              <h5 class="card-header">Ficheiros disponíveis para download</h5>
+              <div class="card-body">
+                <div class="table-responsive text-nowrap">
+                  <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Submetido por</th>
+                        <th>Ficheiro</th>
+                        <th>Permissões</th>
+                  
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
 
-    <section name="descarregar" style="text-align: center";>
-    <div>
-        <h2>Descarregar Ficheiros</h2>
-        <?php
-        for ($a = 2; $a < count($ficheiros); $a++) {
-            ?>
-            <p>
-                <a download="<?php echo $ficheiros[$a] ?>" href="../../assets/ficheiros<?php echo $ficheiros[$a] ?>">
-                    <?php echo $ficheiros[$a] ?>
-                </a>
-            </p>
-            <?php
-        }
-        ?>
-    </div>
-    </section>
-<hr>
+                      <?php
+                      $ficheiros = scandir("../assets/ficheiros");
+                      for ($a = 2; $a < count($ficheiros); $a++) {
+                        ?>
+                        <tr>
+                          <td>
+                            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+                              <li
+                                data-bs-toggle="tooltip"
+                                data-popup="tooltip-custom"
+                                data-bs-placement="top"
+                                class="avatar avatar-xs pull-up"
+                                title="Lilian Fuller"
+                              >
+                                <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
+                              </li>
+                              <!-- Add more li elements for each user -->
+                            </ul>
+                          </td>
+                          <td>
+                            <a href="../assets/ficheiros/<?php echo $ficheiros[$a] ?>">
+                              <?php echo $ficheiros[$a] ?>
+                            </a>
+                          </td>
+                          <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                          <i class="menu-icon tf-icons bx bx-cloud-download"></i>
+</button>
+</td>
+                          
+                          <td>
+                            <div class="dropdown">
+                              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                              </button>
+                              <div class="dropdown-menu">
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                              </div>
+                            </div>
+                          </td>
+                        </tr>
+                      <?php
+                      }
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
 
-    <section name="carregar" style="text-align: center";>
-        <form method="POST" enctype="multipart/form-data" action="upload.php">
-            <input type="file" name="ficheiro">
-            <input type="submit" value="Carregar">
-        </form>
-    </section>
-<hr>
+
+
+<!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="../php/downloadficheiro.php" method="post">
+
+        <div class="mb-3 row">
+                        <label for="html5-password-input" class="col-md-2 col-form-label">Password</label>
+                        <div class="col-md-10">
+                          <input class="form-control" type="password" name="password" id="html5-password-input" />
+                        </div>
+                      </div>
+      
+      
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+      </form>
+    </div>
+  
+  </div>
+</div>
 
 
 
