31318a55cfd7c393cc2d22143adc354e0b50cb94
Ferreira <paulocferreira@deloitte.pt>
2023-12-16 16:09:07 +0000

    mudanças finais na logica de ficheiro - nao vale apena meter input hidden

--- php/postFiles.php
+++ php/postFiles.php
@@ -7,7 +7,6 @@ if (isset($_SESSION["user"])) {
     $user = $_SESSION["user"];
 }
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
 
     if (isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"][0])) {
@@ -21,37 +20,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
             $enviado_em = date("Y-m-d H:i:s");
             $tipo = mime_content_type($_FILES["fileToUpload"]["tmp_name"][$i]);
 
-        // Preparar a consulta SQL para inserir os dados na base de dados
-        $stmt = $pdo->prepare("INSERT INTO ficheiros (nomeficheiro, tipo, dataficheiro, enviado_em) VALUES (?, ?, ?, ?)");
-        $stmt->bindParam(1, $nomeficheiro);
-        $stmt->bindParam(2, $tipo);
-        $stmt->bindParam(3, $dataficheiro, PDO::PARAM_LOB);
-        $stmt->bindParam(4, $enviado_em);
+            // Preparar a consulta SQL para inserir os dados na base de dados
+            $stmt = $pdo->prepare("INSERT INTO ficheiros (nomeficheiro, tipo, dataficheiro, enviado_em) VALUES (?, ?, ?, ?)");
+            $stmt->bindParam(1, $nomeficheiro);
+            $stmt->bindParam(2, $tipo);
+            $stmt->bindParam(3, $dataficheiro, PDO::PARAM_LOB);
+            $stmt->bindParam(4, $enviado_em);
 
-        // Executar a consulta SQL
-        if ($stmt->execute()) {
-            echo "O arquivo $nomeficheiro foi enviado com sucesso e registrado na base de dados.";
-        } else {
-            echo "Erro ao registrar na base de dados.";
-        }
+            // Executar a consulta SQL
+            if ($stmt->execute()) {
+                echo "O arquivo $nomeficheiro foi enviado com sucesso e registrado na base de dados.";
+            } else {
+                echo "Erro ao registrar na base de dados.";
+            }
 
             $fileid = $pdo->lastInsertId();
 
-        // Verificar se o arquivo é privado
-        $isPrivate = isset($_POST['isPrivate']) ? $_POST['isPrivate'] : 0;
-        $password_ficheiro = ($isPrivate == 1) ? $_POST["passwordFicheiro"] : null;
+            // Verificar se o arquivo é privado
+            $isPrivate = isset($_POST['isPrivate'][$i]) ? $_POST['isPrivate'][$i] : 0;
+            $password_ficheiro = ($isPrivate == 1) ? $_POST["passwordFicheiro"][$i] : null;
 
-        $stmt2 = $pdo->prepare("INSERT INTO acessos (userid, fileid, password_ficheiro, publico) VALUES (?, ?, ?, ?)");
-        $stmt2->bindParam(1, $user);
-        $stmt2->bindParam(2, $fileid, PDO::PARAM_LOB);
-        $stmt2->bindParam(3, $password_ficheiro);
-        $stmt2->bindParam(4, $isPrivate);
+            $stmt2 = $pdo->prepare("INSERT INTO acessos (userid, fileid, password_ficheiro, publico) VALUES (?, ?, ?, ?)");
+            $stmt2->bindParam(1, $user);
+            $stmt2->bindParam(2, $fileid, PDO::PARAM_LOB);
+            $stmt2->bindParam(3, $password_ficheiro);
+            $stmt2->bindParam(4, $isPrivate);
 
-        if ($stmt2->execute()) {
-            echo "Sucesso";
-        } else {
-            echo "Erro";
-        }
+            if ($stmt2->execute()) {
+                echo "Sucesso";
+            } else {
+                echo "Erro";
+            }
+        } // Closing brace for the loop
     } else {
         echo "Erro no envio do arquivo.";
     }
